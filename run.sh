861a5d938e007a0b3283b79fc88ba1553a762f2d
Zhihong Ye <yezhihong@gmail.com>
2017-07-26 17:57:12 -0500

    upload the most recent draft and plost 	renamed:    C12_He3_XS_Ratio_MC_XbjJan08_XbjJan08.pdf -> C12_He3_XS_Ratio_MC.pdf 	new file:   He4_He3_XS_Ratio.pdf 	renamed:    He4_He3_XS_Ratio_MC_XbjJan08_XbjJan08.pdf -> He4_He3_XS_Ratio_MC.pdf 	deleted:    He4_He3_XS_Ratio_XbjJan08_XbjJan08.pdf 	new file:   "Icon\r" 	modified:   Plot_XS_25_MC.pdf 	new file:   e08014.aux 	new file:   e08014.log 	modified:   e08014.pdf 	modified:   experiment.tex 	new file:   full_e08014.tex 	modified:   intro.tex 	modified:   results.tex 	new file:   run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+pdflatex e08014.tex
+pdflatex e08014.tex
+bibtex e08014
+bibtex e08014
+pdflatex e08014.tex
+pdflatex e08014.tex
+rm -f *.blg
+rm -f *.bbl
+rm -f *.aux
+rm -f *.log
+rm -f *Notes.bib
+
